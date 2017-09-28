608841de96da16e260262c19562d1c5d5422aa2b
José Luis García <jgarcia@nearshoremx.com>
2017-09-28 17:52:53 -0500

    +Login Success with API Solar

--- app/src/main/java/com/itesm/digital/solar/Proyects.java
+++ app/src/main/java/com/itesm/digital/solar/Proyects.java
@@ -1,12 +1,17 @@
 package com.itesm.digital.solar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Proyects extends AppCompatActivity {
+
+    public SharedPreferences prefs;
+    public String ACTIVE_USERNAME = "", ID_USER="",TOKEN="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +36,15 @@ public class Proyects extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        initValues();
+    }
+
+    private void initValues(){
+
+        prefs = getSharedPreferences("AccessUser", Context.MODE_PRIVATE);
+        ACTIVE_USERNAME = prefs.getString("User", null);
+        ID_USER = prefs.getString("IdUser", null);
+        TOKEN = prefs.getString("Token", null);
     }
 }
