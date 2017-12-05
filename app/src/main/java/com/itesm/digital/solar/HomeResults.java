6e15c7bf2cc940de91febec59158751897653008
JoseloGR <jgarcia@nearshoremx.com>
2017-12-05 05:33:21 -0600

    + Home MD Project

--- app/src/main/java/com/itesm/digital/solar/HomeResults.java
+++ app/src/main/java/com/itesm/digital/solar/HomeResults.java
@@ -1,5 +1,7 @@
 package com.itesm.digital.solar;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -22,11 +24,27 @@ public class HomeResults extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ClearActiveProject();
                 finish();
             }
         });
 
         String ID_PROJECT = getIntent().getExtras().getString("ID_PROJECT");
-        Log.d("Project Selected", ID_PROJECT);
+        SetActiveProject(ID_PROJECT);
+    }
+
+    public void SetActiveProject(String ID_P){
+        SharedPreferences project = getSharedPreferences("ActiveProject", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = project.edit();
+
+        editor.putString("ID_PROJECT", ID_P);
+        editor.apply();
+    }
+
+    public void ClearActiveProject(){
+        SharedPreferences project = getSharedPreferences("ActiveProject", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = project.edit();
+        editor.clear();
+        editor.commit();
     }
 }
--- app/src/main/java/com/itesm/digital/solar/Projects.java
+++ app/src/main/java/com/itesm/digital/solar/Projects.java
@@ -60,6 +60,16 @@ public class Projects extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
