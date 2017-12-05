0139e1f59053aae7af11d3873547d70fd4cae816
JoseloGR <jgarcia@nearshoremx.com>
2017-12-05 06:32:37 -0600

    + ConnectionDrone Card

--- app/src/main/java/com/itesm/digital/solar/HomeResults.java
+++ app/src/main/java/com/itesm/digital/solar/HomeResults.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,7 @@ import com.itesm.digital.solar.Models.Project;
 
 public class HomeResults extends AppCompatActivity {
 
+    String ID_PROJECT="", ID_AREA="1", TOKEN="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,19 @@ public class HomeResults extends AppCompatActivity {
             public void onClick(View view) {
                 ClearActiveProject();
                 Intent intent = new Intent(HomeResults.this, Projects.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        CardView cardDrone = (CardView) findViewById(R.id.dronecardId);
+        cardDrone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeResults.this, ConnectionActivity.class);
+                intent.putExtra("ID_PROJECT", ID_PROJECT);
+                intent.putExtra("ID_AREA", ID_AREA);
+                intent.putExtra("TOKEN", TOKEN);
                 startActivity(intent);
                 finish();
             }
