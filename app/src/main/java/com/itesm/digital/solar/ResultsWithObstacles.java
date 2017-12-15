078248350b387522ec72370526e841fd322af18d
José Luis García <jgarcia@nearshoremx.com>
2017-12-15 09:22:52 -0600

    changes

--- app/src/main/java/com/itesm/digital/solar/ResultsWithObstacles.java
+++ app/src/main/java/com/itesm/digital/solar/ResultsWithObstacles.java
@@ -286,50 +286,7 @@ public class ResultsWithObstacles extends AppCompatActivity implements RecyclerV
 
         private void generateDefaultData() {
             int numSubcolumns = 1;
-            int numColumns = 8;
-            // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
-            List<Column> columns = new ArrayList<Column>();
-            List<SubcolumnValue> values;
-            for (int i = 0; i < numColumns; ++i) {
-
-                values = new ArrayList<SubcolumnValue>();
-                for (int j = 0; j < numSubcolumns; ++j) {
-                    values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
-                }
-
-                Column column = new Column(values);
-                column.setHasLabels(hasLabels);
-                column.setHasLabelsOnlyForSelected(hasLabelForSelected);
-                columns.add(column);
-            }
-
-            data = new ColumnChartData(columns);
-
-            if (hasAxes) {
-                Axis axisX = new Axis();
-                Axis axisY = new Axis().setHasLines(true);
-                if (hasAxesNames) {
-                    axisX.setName("Axis X");
-                    axisY.setName("Axis Y");
-                }
-                data.setAxisXBottom(axisX);
-                data.setAxisYLeft(axisY);
-            } else {
-                data.setAxisXBottom(null);
-                data.setAxisYLeft(null);
-            }
-
-            chart.setColumnChartData(data);
-
-        }
-
-        /**
-         * Generates columns with subcolumns, columns have larger separation than subcolumns.
-         */
-        private void generateSubcolumnsData() {
-            int numSubcolumns = 4;
-            int numColumns = 4;
-            // Column can have many subcolumns, here I use 4 subcolumn in each of 8 columns.
+            int numColumns = 20;
             List<Column> columns = new ArrayList<Column>();
             List<SubcolumnValue> values;
             for (int i = 0; i < numColumns; ++i) {
@@ -365,132 +322,6 @@ public class ResultsWithObstacles extends AppCompatActivity implements RecyclerV
 
         }
 
-        /**
-         * Generates columns with stacked subcolumns.
-         */
-        private void generateStackedData() {
-            int numSubcolumns = 4;
-            int numColumns = 8;
-            // Column can have many stacked subcolumns, here I use 4 stacke subcolumn in each of 4 columns.
-            List<Column> columns = new ArrayList<Column>();
-            List<SubcolumnValue> values;
-            for (int i = 0; i < numColumns; ++i) {
-
-                values = new ArrayList<SubcolumnValue>();
-                for (int j = 0; j < numSubcolumns; ++j) {
-                    values.add(new SubcolumnValue((float) Math.random() * 20f + 5, ChartUtils.pickColor()));
-                }
-
-                Column column = new Column(values);
-                column.setHasLabels(hasLabels);
-                column.setHasLabelsOnlyForSelected(hasLabelForSelected);
-                columns.add(column);
-            }
-
-            data = new ColumnChartData(columns);
-
-            // Set stacked flag.
-            data.setStacked(true);
-
-            if (hasAxes) {
-                Axis axisX = new Axis();
-                Axis axisY = new Axis().setHasLines(true);
-                if (hasAxesNames) {
-                    axisX.setName("Axis X");
-                    axisY.setName("Axis Y");
-                }
-                data.setAxisXBottom(axisX);
-                data.setAxisYLeft(axisY);
-            } else {
-                data.setAxisXBottom(null);
-                data.setAxisYLeft(null);
-            }
-
-            chart.setColumnChartData(data);
-        }
-
-        private void generateNegativeSubcolumnsData() {
-
-            int numSubcolumns = 4;
-            int numColumns = 4;
-            List<Column> columns = new ArrayList<Column>();
-            List<SubcolumnValue> values;
-            for (int i = 0; i < numColumns; ++i) {
-
-                values = new ArrayList<SubcolumnValue>();
-                for (int j = 0; j < numSubcolumns; ++j) {
-                    int sign = getSign();
-                    values.add(new SubcolumnValue((float) Math.random() * 50f * sign + 5 * sign, ChartUtils.pickColor
-                            ()));
-                }
-
-                Column column = new Column(values);
-                column.setHasLabels(hasLabels);
-                column.setHasLabelsOnlyForSelected(hasLabelForSelected);
-                columns.add(column);
-            }
-
-            data = new ColumnChartData(columns);
-
-            if (hasAxes) {
-                Axis axisX = new Axis();
-                Axis axisY = new Axis().setHasLines(true);
-                if (hasAxesNames) {
-                    axisX.setName("Axis X");
-                    axisY.setName("Axis Y");
-                }
-                data.setAxisXBottom(axisX);
-                data.setAxisYLeft(axisY);
-            } else {
-                data.setAxisXBottom(null);
-                data.setAxisYLeft(null);
-            }
-
-            chart.setColumnChartData(data);
-        }
-
-        private void generateNegativeStackedData() {
-
-            int numSubcolumns = 4;
-            int numColumns = 8;
-            // Column can have many stacked subcolumns, here I use 4 stacke subcolumn in each of 4 columns.
-            List<Column> columns = new ArrayList<Column>();
-            List<SubcolumnValue> values;
-            for (int i = 0; i < numColumns; ++i) {
-
-                values = new ArrayList<SubcolumnValue>();
-                for (int j = 0; j < numSubcolumns; ++j) {
-                    int sign = getSign();
-                    values.add(new SubcolumnValue((float) Math.random() * 20f * sign + 5 * sign, ChartUtils.pickColor()));
-                }
-
-                Column column = new Column(values);
-                column.setHasLabels(hasLabels);
-                column.setHasLabelsOnlyForSelected(hasLabelForSelected);
-                columns.add(column);
-            }
-
-            data = new ColumnChartData(columns);
-
-            // Set stacked flag.
-            data.setStacked(true);
-
-            if (hasAxes) {
-                Axis axisX = new Axis();
-                Axis axisY = new Axis().setHasLines(true);
-                if (hasAxesNames) {
-                    axisX.setName("Axis X");
-                    axisY.setName("Axis Y");
-                }
-                data.setAxisXBottom(axisX);
-                data.setAxisYLeft(axisY);
-            } else {
-                data.setAxisXBottom(null);
-                data.setAxisYLeft(null);
-            }
-
-            chart.setColumnChartData(data);
-        }
 
         private int getSign() {
             int[] sign = new int[]{-1, 1};
@@ -503,16 +334,6 @@ public class ResultsWithObstacles extends AppCompatActivity implements RecyclerV
                     generateDefaultData();
                     break;
                 case SUBCOLUMNS_DATA:
-                    generateSubcolumnsData();
-                    break;
-                case STACKED_DATA:
-                    generateStackedData();
-                    break;
-                case NEGATIVE_SUBCOLUMNS_DATA:
-                    generateNegativeSubcolumnsData();
-                    break;
-                case NEGATIVE_STACKED_DATA:
-                    generateNegativeStackedData();
                     break;
                 default:
                     generateDefaultData();
@@ -520,39 +341,7 @@ public class ResultsWithObstacles extends AppCompatActivity implements RecyclerV
             }
         }
 
-        private void toggleLabels() {
-            hasLabels = !hasLabels;
 
-            if (hasLabels) {
-                hasLabelForSelected = false;
-                chart.setValueSelectionEnabled(hasLabelForSelected);
-            }
-
-            generateData();
-        }
-
-        private void toggleLabelForSelected() {
-            hasLabelForSelected = !hasLabelForSelected;
-            chart.setValueSelectionEnabled(hasLabelForSelected);
-
-            if (hasLabelForSelected) {
-                hasLabels = false;
-            }
-
-            generateData();
-        }
-
-        private void toggleAxes() {
-            hasAxes = !hasAxes;
-
-            generateData();
-        }
-
-        private void toggleAxesNames() {
-            hasAxesNames = !hasAxesNames;
-
-            generateData();
-        }
 
         /**
          * To animate values you have to change targets values and then call {@link Chart#startDataAnimation()}
